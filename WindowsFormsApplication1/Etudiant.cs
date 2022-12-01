458f345bbc72abc7d6fcad518485fa9e727b470f
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-12-02 00:05:11 +0100

    .

--- WindowsFormsApplication1/Etudiant.cs
+++ WindowsFormsApplication1/Etudiant.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication1
 {
-    internal class Etudiant
+    
+
+namespace WindowsFormsApplication1
+
+{
+    //Etudiant
+    public class Etudiant
     {
-    }
+        public int id { get; set; }
+        public string nom { get; set; }
+        public string prenom { get; set; }
+
+        public Etudiant( string nom, string prenom)
+        {
+            modelDB.addEtudiant(nom, prenom);
+            this.id = modelDB.getid_byname(nom, prenom);
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+        //getters
+        public int getid()
+        {
+            return this.id;
+        }
+        public string getnom()
+        {
+            return this.nom;
+        }
+        public string getprenom()
+        {
+            return this.prenom;
+        }
+        //setters
+        public void setnom(string nom)
+        {
+            modelDB.updateEtudiant(this.id, nom, this.prenom);
+            this.nom = nom;
+        }
+        public void setprenom(string prenom)
+        {
+            modelDB.updateEtudiant(this.id, this.nom, prenom);
+            this.prenom = prenom;
+        }
+        public void delete()
+        {
+            modelDB.deleteEtudiant(this.id);
+        }
+    }}
+
 }
--- WindowsFormsApplication1/ModelDB.cs
+++ WindowsFormsApplication1/ModelDB.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApplication1
 {
-    internal class ModelDB
+    public class ModelDB
     {
+        SqlConnection con = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\Users\user\Documents\Visual Studio 2010\Projects\WindowsFormsApplication1\WindowsFormsApplication1\Database1.mdf;Integrated Security=True;User Instance=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        public void addEtudiant(string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into Etudiant(nom,prenom) values('" + nom + "','" + prenom + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void deleteEtudiant(int id)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "delete from Etudiant where id=" + id;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void updateEtudiant(int id, string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "update Etudiant set nom='" + nom + "',prenom='" + prenom + "' where id=" + id;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public int getid_byname(string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select id from Etudiant where nom='" + nom + "' and prenom='" + prenom + "'";
+            int id = (int)cmd.ExecuteScalar();
+            con.Close();
+            return id;
+        }
+
+        public DataTable getEtudiants()
+        {
+            DataTable dt = new DataTable();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Etudiant";
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
+        
     }
 }
