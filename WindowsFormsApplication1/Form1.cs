46052ddbfd468b9610a7db60c6f65e2380c06c78
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-12-01 23:02:12 +0100

    .

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Data;
 using System.Windows.Forms;
-using System.Data.SqlClient;
 
 
 namespace WindowsFormsApplication1
@@ -13,35 +12,6 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-
-        ////Pour Access----  using System.Data.OleDb;
-        //OleDbConnection AccesConnection = new OleDbConnection();
-        //OleDbDataAdapter AccesAdapter = new OleDbDataAdapter();
-        //String AccesChaineConnection = "Provider=Microsoft.Jet.OLEDB.4.0; Data Source=|DataDirectory|\\BDAcces.mdb;Persist Security Info=True";
-
-        ////Pour SQL Server----using System.Data.SqlClient;
-        //SqlConnection SQLserverConnection = new SqlConnection();
-        //SqlDataAdapter SQLserverAdapter = new SqlDataAdapter();
-        //String SQLserverChaineConnection = "Data Source=.\\SQLEXPRESS;Initial Catalog=BdSQLserver;Integrated Security=True";
-
-        ////Pour MySQL-----using MySql.Data.MySqlClient;
-        //MySqlConnection MySQLConnection = new MySqlConnection();
-        //MySqlDataAdapter MySQLAdapter = new MySqlDataAdapter();
-        //String MySQLChaineConnection = "server=localhost; user id=root;password= ;database=BdMySQL";
-
-        ////Pour Oracle---------using System.Data.OracleClient;
-        //OracleConnection OracleConnection = new OracleConnection();
-        //OracleDataAdapter OracleAdapter = new OracleDataAdapter();
-        //String OracleChaineConnection = "Data Source=BdOracle;User Id=hr;Password=hr";//;DBA Privilege=SYSDBA
-
-
-
-        static string chaine = @"Data Source=localhost;Initial Catalog=BDGED_copy;Integrated Security=True";
-        //"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\App_Data\VotreBD.mdf;Integrated Security=True;User Instance=True"
-        //"Server=.\SQLEXPRESS; DataBase=VotreBD;USER ID=sa; PASSWORD="
-        static SqlConnection cnx = new SqlConnection(chaine);
-        static SqlCommand cmd = new SqlCommand();
-        static SqlDataAdapter adapter = new SqlDataAdapter(cmd);
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -76,16 +46,6 @@ namespace WindowsFormsApplication1
             cnx.Close();
         }
 
-        private void btnSelect_Click(object sender, EventArgs e)
-        {
-            cnx.Open();
-            cmd.CommandText = "select * from DOSSIER";
-            cmd.Connection = cnx;
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
-            dataGridView1.DataSource = dt;
-            cnx.Close();
-        }
 
         private void Cancel_Click(object sender, EventArgs e)
         {
--- WindowsFormsApplication1/ModelDB.cs
+++ WindowsFormsApplication1/ModelDB.cs
@@ -15,8 +15,52 @@ namespace WindowsFormsApplication1
         SqlCommand cmd = new SqlCommand();
         SqlDataAdapter da = new SqlDataAdapter();
 
-        //Etudiant(id )
-        
+        //Etudiant(id autoincrement, nom,prenom )
+        public void addEtudiant(string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "insert into Etudiant(nom,prenom) values('" + nom + "','" + prenom + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void deleteEtudiant(int id)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "delete from Etudiant where id=" + id;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public void updateEtudiant(int id, string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "update Etudiant set nom='" + nom + "',prenom='" + prenom + "' where id=" + id;
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+        public int getid_byname(string nom, string prenom)
+        {
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select id from Etudiant where nom='" + nom + "' and prenom='" + prenom + "'";
+            int id = (int)cmd.ExecuteScalar();
+            con.Close();
+            return id;
+        }
+
+        public DataTable getEtudiants()
+        {
+            DataTable dt = new DataTable();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Etudiant";
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            con.Close();
+            return dt;
+        }
         
     }
 }
