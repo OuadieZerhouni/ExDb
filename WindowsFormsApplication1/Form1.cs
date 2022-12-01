788a0b183b628a94cb694959adf2ad0d06e2e611
OuadieZerhouni <Ouadie.zerhouni@usmba.ac.ma>
2022-12-01 23:28:24 +0100

    .

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApplication1
         }
         //list of etudiants
         public static List<Etudiant> etudiants = new List<Etudiant>();
+        int IdSelected ;
 
 
         private void Form1_Load(object sender, EventArgs e)
@@ -69,8 +70,60 @@ namespace WindowsFormsApplication1
             Cancel.Enabled = false;
             btnInsert.Enabled = true;
             comboBox1.Enabled = true;
+            
             titleLabel.Text = "Initiale";
 
         }
+        //LoadInsert enable all exept combobox1 and insert ,update delete button
+        private void LoadInsert()
+        {
+            NomTxt.Enabled = true;
+            PrenomTxt.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            Confirm.Enabled = true;
+            Cancel.Enabled = true;
+            btnInsert.Enabled = false;
+            comboBox1.Enabled = false;
+            titleLabel.Text = "Insertion";
+        }
+        //LoadUpdate enable all exept combobox1 and insert ,update delete button
+        private void LoadUpdate()
+        {
+            NomTxt.Enabled = true;
+            PrenomTxt.Enabled = true;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            Confirm.Enabled = true;
+            Cancel.Enabled = true;
+            btnInsert.Enabled = false;
+            comboBox1.Enabled = false;
+            titleLabel.Text = "Modification";
+        }
+        //LoadSelected show info of selected etudiantin combo box1
+        private void LoadSelected()
+        {
+            NomTxt.Enabled = true;
+            Nom
+            PrenomTxt.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            Confirm.Enabled = false;
+            Cancel.Enabled = false;
+            btnInsert.Enabled = true;
+            comboBox1.Enabled = true;
+            titleLabel.Text = "Selection";
+        }
+        //Fill() fill the combobox1 with etudiants
+        private void Fill()
+        {
+            comboBox1.Items.Clear();
+            foreach (Etudiant etudiant in etudiants)
+            {
+                comboBox1.Items.Add(etudiant.getnom() + " " + etudiant.getprenom());
+            }
+        }
+
+
     }
 }
